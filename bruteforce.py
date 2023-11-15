6660a3467df9b1e93b6b1d04b6e22279ee6d5a44
Hangsnara218 <johan.gabrielson@gmail.com>
2023-11-15 21:04:25 +0100

    Add files via upload

---
+++ bruteforce.py
@@ -0,0 +1,15 @@
+import random
+import pyautogui
+
+character="0123456789abcdefghijklmnopqrstuvxyz"
+character_list=list(character)
+
+password=pyautogui.password("Enter password")
+guess_password=''
+while (guess_password!=password):
+    guess_password=random.choices(character_list, k=len(password))
+    print(">>>>"+str(guess_password) + "")
+    
+    if (guess_password==list(password)):
+        print("Your password is: " + "".join(guess_password))
+        break
